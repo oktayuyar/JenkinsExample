51107e9560960e798ff70eab6592b4192b52c040
oktayuyar <oktyuyar@gmail.com>
2019-09-11 10:30:23 +0300

    Jenkins junit example

---
+++ src/main/java/com/oktay/jenkins/TestExample.java
@@ -0,0 +1,15 @@
+package com.oktay.jenkins;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/*
+ *  Created by oktayuyar on 2019-09-11
+ */
+public class TestExample {
+
+    @Test
+    public void checkName(){
+        Assert.assertTrue( "True : ", true);
+    }
+}
